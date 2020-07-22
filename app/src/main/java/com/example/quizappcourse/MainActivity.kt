72adca4f809d6c02aba55cacdf93294f8a01e293
mr <michalrazny2@o2.pl>
2020-07-22 15:14:06 +0200

    - QApp added

--- app/src/main/java/com/example/quizappcourse/MainActivity.kt
+++ app/src/main/java/com/example/quizappcourse/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.quizappcourse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/* Main activity of application
+* */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/quizappcourse/QApp.kt
@@ -0,0 +1,36 @@
+package com.example.quizappcourse
+
+import android.app.Application
+import android.content.Context
+import android.content.res.Resources
+import com.beardedhen.androidbootstrap.TypefaceProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+
+class QApp : Application() {
+
+    // Application class, instead of dependencies injection <?>
+    override fun onCreate(){
+        super.onCreate()
+        TypefaceProvider.registerDefaultIconSets()  // Icons, czcionki
+
+        res = resources
+        ctx = applicationContext
+
+        fData = FirebaseDatabase.getInstance()
+        fAuth = FirebaseAuth.getInstance()
+
+        fUser = fAuth.currentUser
+    }
+
+    companion object{
+        lateinit var ctx: Context
+        lateinit var res: Resources
+
+        lateinit var fData: FirebaseDatabase
+        lateinit var fAuth: FirebaseAuth
+
+        var fUser: FirebaseUser? = null
+    }
+}
